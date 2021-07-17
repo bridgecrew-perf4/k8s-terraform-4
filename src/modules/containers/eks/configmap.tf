a074e2edc7a668be1089205ad0be576725e45759
krishna <you@example.com>
2021-07-17 06:40:00 +0000

    added modular

--- src/modules/containers/eks/configmap.tf
+++ src/modules/containers/eks/configmap.tf
@@ -1,50 +1,16 @@
-data "aws_caller_identity" "current" {}
 
 locals {
-  auth_launch_template_worker_roles = [
-    for index in range(0, var.create_eks ? local.worker_group_launch_template_count : 0) : {
-      worker_role_arn = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/${element(
-        coalescelist(
-          data.aws_iam_instance_profile.custom_worker_group_launch_template_iam_instance_profile.*.role_name,
-          [""]
-        ),
-        index
-      )}"
-      platform = lookup(
-        var.worker_groups_launch_template[index],
-        "platform",
-        local.workers_group_defaults["platform"]
-      )
-    }
-  ]
-
-  auth_worker_roles = [
-    for index in range(0, var.create_eks ? local.worker_group_count : 0) : {
-      worker_role_arn = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/${element(
-        coalescelist(
-          aws_iam_instance_profile.workers.*.role,
-          data.aws_iam_instance_profile.custom_worker_group_iam_instance_profile.*.role_name,
-          [""]
-        ),
-        index,
-      )}"
-      platform = lookup(
-        var.worker_groups[index],
-        "platform",
-        local.workers_group_defaults["platform"]
-      )
+  auth_worker_roles = [{
+      worker_role_arn = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/${var.iam-role-name-for-workers}"
+      platform = "linux"
     }
   ]
 
   # Convert to format needed by aws-auth ConfigMap
   configmap_roles = [
-    for role in concat(
-      local.auth_launch_template_worker_roles,
-      local.auth_worker_roles,
-    ) :
+    for role in 
+      local.auth_worker_roles:
     {
-      # Work around https://github.com/kubernetes-sigs/aws-iam-authenticator/issues/153
-      # Strip the leading slash off so that Terraform doesn't think it's a regex
       rolearn  = replace(role["worker_role_arn"], replace(var.iam_path, "/^//", ""), "")
       username = role["platform"] == "fargate" ? "system:node:{{SessionName}}" : "system:node:{{EC2PrivateDNSName}}"
       groups = tolist(concat(
@@ -60,32 +26,15 @@ locals {
 }
 
 resource "kubernetes_config_map" "aws_auth" {
-  count      = var.create_eks && var.manage_aws_auth ? 1 : 0
-  depends_on = [null_resource.wait_for_cluster[0]]
-
   metadata {
     name      = "aws-auth"
     namespace = "kube-system"
-    labels = merge(
-      {
-        "app.kubernetes.io/managed-by" = "Terraform"
-        # / are replaced by . because label validator fails in this lib 
-        # https://github.com/kubernetes/apimachinery/blob/1bdd76d09076d4dc0362456e59c8f551f5f24a72/pkg/util/validation/validation.go#L166
-        "terraform.io/module" = "terraform-aws-modules.eks.aws"
-      },
-      var.aws_auth_additional_labels
-    )
   }
 
   data = {
     mapRoles = yamlencode(
-      distinct(concat(
-        local.configmap_roles,
-        var.map_roles,
-      ))
+        local.configmap_roles
     )
-    mapUsers    = yamlencode(var.map_users)
-    mapAccounts = yamlencode(var.map_accounts)
   }
 }
 
